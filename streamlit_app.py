cf6147b094fa3e5a8be160de64a46cdcc4756472
Christian Bernini <cgbas@users.noreply.github.com>
2024-12-03 20:33:43 -0300

    chore: indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,15 +23,15 @@ multi = '''
         Para executar o código localmente, você pode seguir os passos abaixo:
 
         0. Clone o repositório:  
-                > git clone https://github.com/GustavoHenriqueDeCarvalho/tech-fase4.git
+        > git clone https://github.com/GustavoHenriqueDeCarvalho/tech-fase4.git
         1. Crie um ambiente virtual python:  
-                > python -m venv myenv
+        > python -m venv myenv
         2. Ative o ambiente virtual:  
-                > myenv\Scripts\activate
+        > myenv\Scripts\activate
         3. Instale as bibliotecas necessárias:  
-                > pip install -r requirements.txt
+        > pip install -r requirements.txt
         4. Execute o código:  
-                > streamlit run streamlit_app.py
+        > streamlit run streamlit_app.py
 '''
 st.sidebar.markdown(multi)
 
