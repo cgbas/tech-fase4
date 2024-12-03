22a373c3afb7cd7c8c763220072e0d4fee99e9f2
Christian Bernini <cgbas@users.noreply.github.com>
2024-12-03 20:18:22 -0300

    edit: backout

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ st.sidebar.markdown('''
 st.sidebar.markdown("Here's a bouquet &mdash;\
             :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 
-multi = '''If you end a line with two spaces,  
+multi = '''If you end a line with two spaces,
 a soft return is used for the next line.
 
 Two (or more) newline characters in a row will result in a hard return.
