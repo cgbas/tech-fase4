1af5caffc0d83f9818318cef6081ec39e9ac1a11
Christian Bernini <cgbas@users.noreply.github.com>
2024-12-03 20:32:35 -0300

    chore: teste

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,16 +21,17 @@ option = st.sidebar.selectbox(
 
 multi = '''  
         Para executar o código localmente, você pode seguir os passos abaixo:
-        Clone o repositório:
-        > git clone https://github.com/GustavoHenriqueDeCarvalho/tech-fase4.git
+
+        0. Clone o repositório:
+                > git clone https://github.com/GustavoHenriqueDeCarvalho/tech-fase4.git
         1. Crie um ambiente virtual python:
-        > python -m venv myenv
+                > python -m venv myenv
         2. Ative o ambiente virtual:
-        > myenv\Scripts\activate
+                > myenv\Scripts\activate
         3. Instale as bibliotecas necessárias:
-        > pip install -r requirements.txt
+                > pip install -r requirements.txt
         4. Execute o código:
-        > streamlit run streamlit_app.py
+                > streamlit run streamlit_app.py
 '''
 st.sidebar.markdown(multi)
 
